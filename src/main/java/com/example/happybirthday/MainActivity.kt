64a276137088f79343c251d3ff4dbf004fda4119
Kaipa Rajeswara Reddy <57223067+Rajesh-dot@users.noreply.github.com>
2021-01-13 16:49:45 +0530

    Add files via upload

---
+++ src/main/java/com/example/happybirthday/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.happybirthday
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.SeekBar
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val rollButton = findViewById<Button>(R.id.rollButton)
+        val resultsTextView = findViewById<TextView>(R.id.resultsTextView)
+        val seekBar = findViewById<SeekBar>(R.id.seekBar)
+
+        rollButton.setOnClickListener {
+            val rand = java.util.Random().nextInt(seekBar.progress)+1
+            resultsTextView.text = rand.toString()
+        }
+    }
+}
